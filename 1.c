cedb977fc16f44802674c54b708fbbe99a2c9dd5
balakandhasamy <30549603+balakandhasamy@users.noreply.github.com>
2017-07-29 16:57:40 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a,i,j,b,n;
+printf("enter the integer");
+scanf("%d",&n);
+if(n<0.0)
+{
+printf("enter the negative integer");
+}
+else if(n>0.0)
+{
+printf("enter the positive integer");
+}
+else
+{
+pritf("enter the 0.");
+}
+return 0;
+}
